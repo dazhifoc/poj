8db965f78143261972594dbc3fcb8f0bacb890f3
chengzhi <luochenghzhi.mail@gmail.com>
2014-12-27 21:31:08 +0800

    poj1011,当搜索错误时,回退换个分支 贪心,无法通过第4组数据

--- 1011/sticks.cpp
+++ 1011/sticks.cpp
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 int join_sticks(int num, int* sticks);
 int cmp ( const void *a , const void *b );
-bool check_stick(int len, int num, int* sticks);
+bool check_len(int len, int num, int* sticks);
+bool pk_stick(int num, int* sticks, int remain, int head, int &count);
 int main(int argc, char *argv[])
 {
     int num;
@@ -34,47 +35,67 @@ int join_sticks(int num, int* sticks)
     for (int i = mins; i < maxs; i++) {
         int a = sum / i;
         if(a * i == sum){
-            std::cout << i << std::endl;
+            // std::cout << "try:"<< i << std::endl; // test
             int sticks_cp[64];
             for (int i = 0; i < num; i++) {
                 sticks_cp[i] = sticks[i];
             }
-            if (check_stick(i, num, sticks_cp)){
+            if (check_len(i, num, sticks_cp)){
                 return i;
             }
-            std::cout << std::endl;
+            // std::cout << std::endl; // test
         }
     }
     // test
     return sum;
 }
-bool check_stick(int len, int num, int* sticks)
+bool check_len(int len, int num, int* sticks)
 {
-    std::cout << sticks[0] << " ";
-    int remain = len - sticks[0]; //
+    // std::cout << sticks[0] << " ";          // test
+    int remain = len - sticks[0]; //第一根棍子一定要用最大的
     sticks[0] = 0;
     int count = 1;
-    for (int i = 1; i < num; i++) {
-        if(remain >= sticks[i]){
-            remain = remain - sticks[i];
-            std::cout << sticks[i] << " ";
-            sticks[i] = 0;
-            count++;
-        }
-    }
-    if(remain == 0){
-        std::cout << std::endl;
-        qsort(sticks, num, sizeof(int), cmp);
+    int head = 1;
+    bool result = pk_stick(num, sticks, remain, head, count);
+
+    if(result){
+        // std::cout << std::endl;               // test
+        qsort(sticks, num, sizeof(int), cmp);    // 贪心,成功拼接的棍子不会拆开
         if(sticks[0] == 0){
             return true;
         }
-        return check_stick(len, num - count, sticks);
+        return check_len(len, num - count, sticks);
     }
     else
-        std::cout << remain << std::endl;
+        //        std::cout << remain << std::endl;
         return false;
 }
 int cmp ( const void *a , const void *b )
 {
     return *(int *)b - *(int *)a;
+}
+bool pk_stick(int num, int* sticks, int remain, int head, int &count)
+{
+    if(remain == 0){
+        return true;
+    }
+    // 选择一下个棍子
+    for (int i = head; i < num; i++) {
+        if(remain >= sticks[i]){
+            bool one_try =  pk_stick(num, sticks, remain - sticks[i], i +1, count);
+            if(one_try){
+                // std::cout << sticks[i] << " "; // test
+                sticks[i] = 0; // 被用了
+                count++;
+                return true;
+            }
+            else{
+                while (sticks[i]==sticks[i+1]) {
+                    i++;
+                }
+                ;               // next try
+            }
+        }
+    }
+    return false;
 }
