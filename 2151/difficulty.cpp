c24db9d70db864798336dbb40385df01e77afea6
chengzhi <luochenghzhi.mail@gmail.com>
2014-12-29 20:58:44 +0800

    dp

--- 2151/difficulty.cpp
+++ 2151/difficulty.cpp
@@ -1,21 +1,41 @@
 // 日 12/28 17:26:41 2014
 #include <iostream>
+#include <iomanip>
 #define M 30
 #define T 1000
 using namespace std;
-double check_difficulty(double p[T][M], int t, int m){
+double check_difficulty(double p[T][M], int t, int m, int n){
     // 每队分别计算
-    double dp[T][M][M];
+    double dp[T][M+1][M+1] = {0};
     for (int team = 0; team < t; team++) {
         double *p_each = &p[team][0];
-        double *dp_each = &dp[team][0][0];
-        for (int j = 0; j < m; j++) {
-            cout << p_each[j] << " ";
+        // dp[team][0][0],表示共0题,做对0题的的概率 = 1
+        dp[team][0][0] = 1;
+        // dp[team][j][k],表示共j题,做对k题
+        for (int j = 1; j <= m; j++) {
+            // 都没做对,j-1个问题中做对0个,第j个也没做对
+            dp[team][j][0] = dp[team][j-1][0] * (1-p_each[j-1]);
+            for (int k = 0; k <= m; k++) {
+                // j-1个中做对k个,第j个没做对,j-1个中做对k-1个,第j个做对
+                dp[team][j][k] = dp[team][j-1][k] * (1-p_each[j-1]) + dp[team][j-1][k-1] * p_each[j-1];
+            }
         }
-        cout << endl;
     }
     // 每队至少答对一题
-    return 0;
+    double least1 = 1;
+    for (int team = 0; team < t; team++) {
+        least1 *= (1 - dp[team][m][0]);
+    }
+    // 每队做对1~n-1
+    double one_to_n = 1;
+    for (int team = 0; team < t; team++) {
+        double one_team = 0;
+        for (int k = 1; k < n; k++) {
+            one_team += dp[team][m][k];
+        }
+        one_to_n *= one_team;
+    }
+    return least1 - one_to_n;
 }
 int main(int argc, char *argv[])
 {
@@ -30,7 +50,7 @@ int main(int argc, char *argv[])
                 cin >> p[i][j];
             }
         }
-        cout << check_difficulty(p,t,m) << endl;
+        cout << setprecision(3) <<check_difficulty(p,t,m,n) << endl;
         cin >> m >> t >> n;
     }
     return 0;
